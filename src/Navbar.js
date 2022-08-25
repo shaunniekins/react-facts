6f842b1cd7ca4f4ce2044001113bb3cce817471a
Shaun Niel <94605637+shaunniekins@users.noreply.github.com>
2022-08-25 10:46:39 +0800

    Initial Commit

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+// import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div id="nav--logo">
+                <img src="./logo512.png" alt="React-logo" id="nav--logo_icon" />
+                <h3 id="nav--logo_text">ReactFacts</h3>
+            </div>
+            <h4 id="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
